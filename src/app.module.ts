3c1e374f7d5a0e488e990190fdecf25ed08032bf
duccanhole <duccanhole@gmail.com>
2024-02-19 21:29:57 +0700

    connect to mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
+const DB_URL = process.env.DB_URL;
+const DB_CONNECTION = process.env.DB_CONNECTION;
+
+console.log(DB_URL, DB_CONNECTION);
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('DB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
